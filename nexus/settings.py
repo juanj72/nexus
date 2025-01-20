118042b0c565626f505039ac7189553d3571c704
juanj72 <juan.jara@campusucc.edu.co>
2025-01-20 15:35:51 -0500

    chore: secret key changed

--- nexus/settings.py
+++ nexus/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 PROBE_LIBRARY_PATH = os.getenv('PROJ_DB')
