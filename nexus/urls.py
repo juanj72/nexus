ef76241f98b03f2b80667b2f9df97b37816b0061
juanj72 <juan.jara@campusucc.edu.co>
2025-01-21 22:55:25 -0500

    chore: route documentation changed

--- nexus/urls.py
+++ nexus/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     path('user/', include('user.urls')),
     #swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json/',schema_view.without_ui(cache_timeout=0),name='schema-json'),
 ]
